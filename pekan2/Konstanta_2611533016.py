3b88786d277b5540f12cab72bea0a24ec5ddb89c
gilangmiftahulriski <gilangsikumbang1@gmail.com>
2026-09-14 07:42:41 +0700

    pekan2

--- pekan2/Konstanta_2611533016.py
+++ pekan2/Konstanta_2611533016.py
@@ -3,8 +3,8 @@
 # nama variabel ditambah 4 digit nim terakhir contoh:  jari_3016
 
 from typing import Final
-PI: Final = 3.14
-print("pi: %f" % (PI))
-jari_3016 = float("input'Masukkan nilai jari-jari: ")
-luas_3016 = PI * jari_3016 * jari_3016
+PI_3016: Final = 3.14
+print("pi: %f" % (PI_3016))
+jari_3016 = float(input("Masukkan nilai jari-jari: "))
+luas_3016 = PI_3016 * jari_3016 * jari_3016
 print("Luas lingkaran dengan jari-jari %.2f adalah %.2f" % (jari_3016, luas_3016))
